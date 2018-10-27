49e3215534af1e1b3315facfe5af48a9eb34bede
iamtheluiz <iamtheluiz.dev@gmail.com>
2018-10-27 18:26:06 -0300

    Mudança de cor a cada turno

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //Váriaveis Globais
-var turno = "x";	
+var turno = "x";
+var body = document.querySelector("body");
 
 //Funções do Jogo
 
@@ -9,13 +10,20 @@ function fazer_jogada(isto){
 	if(isto.innerHTML == ""){
 		//Campo não foi selecionado
 		if(turno == "x"){
+			body.setAttribute("class","red_player");
+			isto.setAttribute("content","x");
 			isto.innerHTML = "<div class='close'></div>";
 			turno = "o";
 		}else{
+			body.setAttribute("class","blue_player");
+			isto.setAttribute("content","o");
 			isto.innerHTML = "<div class='circle'></div>";
 			turno = "x";
 		}
 	}else{
 		//Campo já foi selecionado
 	}
+}
+function verificar_jogo(){
+
 }
